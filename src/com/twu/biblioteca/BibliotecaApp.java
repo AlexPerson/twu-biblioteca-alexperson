3c829bc4605e50cd4f3642589664f8ffb72dbbcc
AlexPerson <alex.person@thoughtworks.com>
2020-02-07 13:48:03 -0600

    1.4 Creates a menu where a user can select one option, List of Books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BibliotecaApp {
     ArrayList<Book> bookList = new ArrayList<Book>();
@@ -14,10 +15,30 @@ public class BibliotecaApp {
             System.out.println(book.title);
         }
     }
+    public void showMenu() {
+        System.out.println("\nSelect an option from the menu below:\n");
+        System.out.println("1) List of Books");
+        getUserInput();
+    }
+    public void getUserInput() {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("\n" +
+                "Selected option: ");
+
+        int option = Integer.parseInt(myObj.nextLine());  // Read user input
+
+        switch (option) {
+            case 1:
+                displayBooks();
+                break;
+            default:
+                System.out.println("Error: " + option + " is not a valid option!");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great books in Bangalore!");
         BibliotecaApp app = new BibliotecaApp();
         app.setUpBooks();
-        app.displayBooks();
+        app.showMenu();
     }
 }
